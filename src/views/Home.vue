619bee7656e1e7ac8c81715f426c7651bed13426
amilford87 <amilford87@gmail.com>
2020-07-27 22:31:54 -0400

    added axios and newsapi

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,39 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <div class="row mt-5">
+        <div class="col-md-5">
+          test
+        </div>
+        <div class="col-md-7">
+          test
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import axios from 'axios'
 
 export default {
   name: 'Home',
+  data() {
+    return {
+      news: []
+    }
+  },
   components: {
     HelloWorld
+  },
+  created() {
+    axios.get('http://newsapi.org/v2/everything?q=bitcoin&from=2020-07-28&sortBy=popularity&apiKey=422ecef7ebfe4adeacd5e38b6222a0fb')
+    .then(res => this.news = res.data)
+    .catch(err => console.log(err));
+
+    // console.log(this.news);
   }
 }
 </script>
